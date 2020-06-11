12eaadd48f3ff9a9f8c919532580b8050d34e8f4
LittleBoat <abe.chua@qq.com>
2020-06-12 04:45:22 +1200

    update Context's send function name

--- carlolib/contextpool.go
+++ carlolib/contextpool.go
@@ -4,16 +4,16 @@ import "sync"
 
 var contextPool = newContextPool()
 
-type ContextFunc func(seq uint32, buf []byte) error
+type SendFunc func(seq uint32, buf []byte) error
 
 type Context struct {
-	seq uint32
-	buf []byte
-	fn  ContextFunc
+	seq  uint32
+	buf  []byte
+	send SendFunc
 }
 
 func (c *Context) Body() []byte           { return c.buf }
-func (c *Context) Reply(buf []byte) error { return c.fn(c.seq, buf) }
+func (c *Context) Reply(buf []byte) error { return c.send(c.seq, buf) }
 
 type ContextPool struct {
 	sp sync.Pool
@@ -23,13 +23,13 @@ func newContextPool() *ContextPool {
 	return &ContextPool{sp: sync.Pool{}}
 }
 
-func (p *ContextPool) acquire(fn ContextFunc, seq uint32, buf []byte) *Context {
+func (p *ContextPool) acquire(send SendFunc, seq uint32, buf []byte) *Context {
 	v := p.sp.Get()
 	if v == nil {
 		v = &Context{}
 	}
 	ctx := v.(*Context)
-	ctx.fn = fn
+	ctx.send = send
 	ctx.seq = seq
 	ctx.buf = buf
 	return ctx
