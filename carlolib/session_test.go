9cee4b2e571d89eb31aaf86fb35fd6a9ab849c0d
LittleBoat <abe.chua@qq.com>
2020-06-13 01:42:08 +1200

    remove poolMetrics

--- carlolib/session_test.go
+++ carlolib/session_test.go
@@ -64,11 +64,6 @@ func TestSessionConn(t *testing.T) {
 		require.NoError(t, err)
 		require.EqualValues(t, strconv.AppendUint(nil, uint64(i), 10), buf[:n])
 	}
-
-	t.Logf("Timer Pool => new:%d,reuse:%d,putback:%d", timerPool.m.na, timerPool.m.nr, timerPool.m.np)
-	t.Logf("Context Pool => new:%d,reuse:%d,putback:%d", contextPool.m.na, contextPool.m.nr, contextPool.m.np)
-	t.Logf("PendingRequest Pool => new:%d,reuse:%d,putback:%d", pendingRequestPool.m.na, pendingRequestPool.m.nr, pendingRequestPool.m.np)
-	t.Logf("PendingWrite Pool => new:%d,reuse:%d,putback:%d", pendingWritePool.m.na, pendingWritePool.m.nr, pendingWritePool.m.np)
 }
 
 func TestSession(t *testing.T) {
@@ -106,10 +101,4 @@ func TestSession(t *testing.T) {
 
 	require.NoError(t, conn.Close())
 	require.NoError(t, bob.Close())
-
-	t.Logf("Timer Pool => new:%d,reuse:%d,putback:%d", timerPool.m.na, timerPool.m.nr, timerPool.m.np)
-	t.Logf("Context Pool => new:%d,reuse:%d,putback:%d", contextPool.m.na, contextPool.m.nr, contextPool.m.np)
-	t.Logf("PendingRequest Pool => new:%d,reuse:%d,putback:%d", pendingRequestPool.m.na, pendingRequestPool.m.nr, pendingRequestPool.m.np)
-	t.Logf("PendingWrite Pool => new:%d,reuse:%d,putback:%d", pendingWritePool.m.na, pendingWritePool.m.nr, pendingWritePool.m.np)
-
 }
